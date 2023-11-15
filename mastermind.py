41d7dbe38e5f9c7e45e89034888422b24851625b
“Khunakorn Pattayakorn” <khunakornp.@gmail.com>
2023-11-15 11:35:43 +0700

    Added board state and board set up

--- mastermind.py
+++ mastermind.py
@@ -3,20 +3,44 @@ class Combination:
     def __init__(self, color, length):
         self.__color = color
         self.__length = length
-
+        self.__code = []
     @property
     def color(self):
         return self.__color
+
+    @color.setter
+    def color(self, color):
+        self.__color = color
 
     @property
     def length(self):
         return self.__length
 
+    @length.setter
+    def length(self, length):
+        self.__length = length
+
     @property
-    def combination(self):
+    def code(self):
         comb_list = []
         for i in range(self.__length):
-            rand_color = random.randint(0, self.__length)
+            rand_color = random.randint(0, self.__color)
             comb_list.append(rand_color)
-        return comb_list
+        self.__code = comb_list
+        return self.__code
+
+
+class Board:
+    def __init__(self):
+        self.combination = Combination(4,6)
+
+    def set_board(self):
+        length = input("Enter code length: ")
+        color = input("Enter the max amount of colors the code can contain: ")
+        self.combination.length = length
+        self.combination.color = color
+
+    def guess(self, user_input):
+        for i in self.combination.code:
+            pass
 
